d339d0a12216922633df090ea942242775784ed9
Sebastian Fischer <mail@sebfisch.de>
2023-02-24 11:20:38 +0100

    refactor simulation and add performance figures

--- src/main/java/sebfisch/accounts/Simulation.java
+++ src/main/java/sebfisch/accounts/Simulation.java
@@ -14,33 +14,26 @@ import java.util.random.RandomGeneratorFactory;
 
 public class Simulation<A extends Bank.Account> {
 
-  static final int STARTING_BALANCE = 100;
+  static final int NUMBER_OF_THREADS = 16;
+  static final long NUMBER_OF_TASKS = 10000000;
+  static final int NUMBER_OF_ACCOUNTS = 10;
+  static final int STARTING_BALANCE = 100000;
 
-  record Config(
-      int numberOfAccounts,
-      int numberOfReads,
-      int numberOfWrites) {
+  record Scenario(long readShare, long writeShare) {
+    long numberOfReads() {
+      return readShare * NUMBER_OF_TASKS / (readShare + writeShare);
+    }
+
+    long numberOfWrites() {
+      return writeShare * NUMBER_OF_TASKS / (readShare + writeShare);
+    }
   }
 
-  static final Config READ_ONLY = new Config(
-      10,
-      20000,
-      0);
-
-  static final Config WRITE_ONLY = new Config(
-      10,
-      0,
-      20000);
-
-  static final Config MORE_READS = new Config(
-      10,
-      20000,
-      200);
-
-  static final Config MORE_WRITES = new Config(
-      10,
-      200,
-      20000);
+  static final Scenario READ_ONLY = new Scenario(1, 0);
+  static final Scenario WRITE_ONLY = new Scenario(0, 1);
+  static final Scenario MORE_READS = new Scenario(100, 1);
+  static final Scenario MORE_WRITES = new Scenario(1, 100);
+  static final Scenario BOTH_EQUALLY = new Scenario(1, 1);
 
   public static void main(String[] args) throws InterruptedException {
     List<Supplier<Bank<?>>> bankSuppliers = List.of(
@@ -50,31 +43,35 @@ public class Simulation<A extends Bank.Account> {
         StampedBank::new //
     );
 
-    List<Config> configs = List.of(
+    List<Scenario> configs = List.of(
         READ_ONLY,
         WRITE_ONLY,
         MORE_READS,
-        MORE_WRITES //
+        MORE_WRITES,
+        BOTH_EQUALLY //
     );
 
-    for (var newBank : bankSuppliers) {
-      for (var config : configs) {
+    for (var config : configs) {
+      System.out.println(config);
+      for (var newBank : bankSuppliers) {
         new Simulation<>(config, newBank.get()).run();
       }
     }
   }
 
-  private final Config config;
+  private final Scenario config;
   private final Bank<A> bank;
 
   private final List<A> accounts = new ArrayList<>();
   private final RandomGenerator rgen = RandomGeneratorFactory.getDefault().create();
 
-  Simulation(Config config, Bank<A> bank) {
+  Simulation(Scenario config, Bank<A> bank) {
+    System.out.println(bank.getClass().getSimpleName());
+
     this.config = config;
     this.bank = bank;
 
-    for (int i = 0; i < config.numberOfAccounts(); i++) {
+    for (int i = 0; i < NUMBER_OF_ACCOUNTS; i++) {
       A account = bank.createAccount();
       account.deposit(STARTING_BALANCE);
       accounts.add(account);
@@ -86,18 +83,18 @@ public class Simulation<A extends Bank.Account> {
   }
 
   void run() throws InterruptedException {
-    System.out.println("%s %s".formatted(bank.getClass().getSimpleName(), config));
     runOnce();
     runOnce();
     runOnce();
   }
 
   void runOnce() throws InterruptedException {
-    ExecutorService pool = Executors.newCachedThreadPool();
+    ExecutorService pool = Executors.newFixedThreadPool(NUMBER_OF_THREADS);
     LongAdder observedBalance = new LongAdder();
+    LongAdder crashedTransfers = new LongAdder();
 
     Instant start = Instant.now();
-    accessAccounts(pool, observedBalance);
+    accessAccounts(pool, observedBalance, crashedTransfers);
     pool.shutdown();
     pool.awaitTermination(10, TimeUnit.SECONDS); // should finish sooner
     long durationInMs = Duration.between(start, Instant.now()).toMillis();
@@ -105,13 +102,13 @@ public class Simulation<A extends Bank.Account> {
     if (!pool.isTerminated()) {
       System.out.println("did not terminate after %dms".formatted(durationInMs));
     } else {
-      System.out.println("observed balance is %d after %dms"
-          .formatted(observedBalance.sum(), durationInMs));
+      System.out.println("observed balance is %d after %d crashed transfers and %dms"
+          .formatted(observedBalance.sum(), crashedTransfers.sum(), durationInMs));
     }
   }
 
-  void accessAccounts(ExecutorService pool, LongAdder observedBalance) {
-    for (int i = 0; i < config.numberOfWrites(); i++) {
+  void accessAccounts(ExecutorService pool, LongAdder observedBalance, LongAdder crashedTransfers) {
+    for (long i = 0; i < config.numberOfWrites(); i++) {
       final A from = randomAccount();
       A other = randomAccount();
       while (from == other) {
@@ -122,12 +119,12 @@ public class Simulation<A extends Bank.Account> {
         try {
           bank.transfer(from, to, 1);
         } catch (InsufficientFundsException e) {
-          // ignore
+          crashedTransfers.increment();
         }
       });
     }
 
-    for (int i = 0; i < config.numberOfReads(); i++) {
+    for (long i = 0; i < config.numberOfReads(); i++) {
       final A account = randomAccount();
       pool.execute(() -> {
         observedBalance.add(account.balance());
